80c4af3b64ec4fc865c62741514959d12f2e7189
Clicode <kou.101530@gmail.com>
2025-09-27 14:22:29 +0900

    add: create botton for switch home and game

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import GameScene from './components/GameScene';
+import NavButton from '@/components/NavButton';
 
 export default function GamePage() {
   return (
-    <GameScene />
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      <GameScene />
+      <NavButton href="/home">Back to Home</NavButton>
+    </div>
   );
 }
